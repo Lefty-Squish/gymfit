783f5cfb4b3b9c6c8d87869ddf964503d4ab0d1c
Tom <tom@mycosupport.co.uk>
2022-07-21 12:25:22 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Gym Fit</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&family=Space+Mono&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+    <link href="https://github.com/Lefty-Squish/TestSite.git"/>
+    <link href="./main.css" rel="stylesheet"/>
+    <link href="main.js">
+    <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css"/>
+    <link href="slick-1.8.1/slick/slick.js" rel="script">
+    <link href="https://github.com/jquery/jquery.git">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+
+
+    <body>
+        <section class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="topBar">
+                        <img src="Images/Gym-Fit.svg" alt="Gym-Fit Logo" id="gymFitLogo">
+                        <nav>
+                            <p id="home">Home</p>
+                            <p>About</p>
+                            <p>Exercises</p>
+                            <p>Contact</p>
+                        </nav>
+                        <button type="button" name="Sign Up" class="signUp">Sign Up</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="frontText">
+                            <img src="Images/health&fitness_logo.png" alt="Health And fitness">
+                            <h1>Don't wish for it, Work for it!</h1>
+                            <p>
+                                The individual who says it is not possible should move out of the way of those doing it.
+                                Most people fail, not because of lack of desire, but, because of lack of commitment
+                            </p>
+                        </div>
+                        <button type="button" name="Explore Exercises" id="exploreExercises">Explore Exercises</button>
+                    </div>
+                    <div class="col-6">
+                        <img src="Images/Hero%20Img.jpg" alt="muscular man" id="hfImg">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container" id="section2">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="learn">
+                            <h2><strong>Learn the exercises you should know</strong></h2>
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <div class="search">
+                            <label for="searchExercises">
+                                <input type="search" name="Search Exercises" placeholder="Search Exercises..." id="searchExercises">
+                            </label>
+                            <button type="submit" class="submitBTN">Submit</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="exerciseBoxes">
+                        <div class="box1">
+                            <img src="Images/dumbbell.png" alt="Dumbbell">
+                            <p>Shoulders</p>
+                        </div>
+                        <div class="box1">
+                            <img src="Images/dumbbell.png" alt="Dumbbell">
+                            <p>Upper Arms</p>
+                        </div>
+                        <div class="box1">
+                            <img src="Images/dumbbell.png" alt="Dumbbell">
+                            <p>Upper Legs</p>
+                        </div>
+                        <div class="box1">
+                            <img src="Images/dumbbell.png" alt="Dumbbell">
+                            <p>Waist</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+
+    <footer>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row" id="bottomBanner">
+                    <div class="col-3">
+                        <div class="loremIpsum">
+                            <img src="Images/Gym-Fit_White.svg" alt="Gym Fit Logo">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Lectus diam ut neque,
+                                phareta. Porta ac nunc at sed eget sed
+                                sed.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="links">
+                            <ul aria-label="Links">
+                                <li>About</li>
+                                <li>Exercises</li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="Company">
+                            <ul aria-label="Company">
+                                <li>Terms & Conditions</li>
+                                <li>Privacy Policy</li>
+                                <li>Careers</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="getInTouch">
+                            <ul aria-label="Get in touch">
+                                <li>
+                                    Dovercourt, Harwich
+                                    <br>
+                                    Essex, CO12
+                                </li>
+                                <li>(+44) 7894 1236</li>
+                                <li>info@gymfit.co.uk</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="copyright">
+                        <p>
+                            <strong><span>&copy</span> Gym-Fit.co.uk <span>All rights reserved</span></strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
+    </body>
+</html>
